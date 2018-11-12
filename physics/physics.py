495dbb448ce7316fd361e6a1660a84fbb075cf7d
Nathan Allen <nathan@allenhub.com>
2018-11-12 13:22:27 -0500

    improved figure eight math, added link to desmos graph

--- physics/physics.py
+++ physics/physics.py
@@ -1,12 +1,14 @@
 
-WIDTH = 400
-HEIGHT = 200
+
+# https://www.desmos.com/calculator/drneqsjl2z
+WIDTH = 4
+HEIGHT = 2
 
 
 def falling(r, velocity):
 
 
 def calculate_posn(r):
-    x = 600 + math.sin(r / WIDTH) * HEIGHT
-    y = HEIGHT + math.sin(r / HEIGHT) * 60
+    x = math.sin(r / WIDTH) * HEIGHT
+    y = math.sin(r / HEIGHT) * .6
     return x, y
