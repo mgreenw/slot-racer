ad3b1ef0d070e572922972b13d50faf389587185
Nathan Allen <nathan@allenhub.com>
2018-11-12 13:41:32 -0500

    added falling function based on figure eight track

--- physics/physics.py
+++ physics/physics.py
@@ -1,11 +1,14 @@
-
-
-# https://www.desmos.com/calculator/drneqsjl2z
+# Nathan Allen
+#
+# https://www.desmos.com/calculator/fgf5a3kiah
 WIDTH = 4
 HEIGHT = 2
 
 
 def falling(r, velocity):
+    x_comp, y_comp = math.cos(r / WIDTH) * 2, math.cos(r / HEIGHT) * .6
+    threshold = math.sqrt(x_comp << 2 + y_comp << 2)
+    return velocity >= threshold
 
 
 def calculate_posn(r):
