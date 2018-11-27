10ab88e475e1958412c7a7a8aa6b0f2dea374673
Nathan Allen <nathan@allenhub.com>
2018-11-27 18:01:52 -0500

    fixed physics references, test

--- slot_racer/game/physics/physics.py
+++ slot_racer/game/physics/physics.py
@@ -3,15 +3,13 @@
 
 import math
 
-SOCKET_WIDTH = .5
+SOCKET_WIDTH = 1
 TRACK_WIDTH = 200.0
 MAX_SPEED = .5
 BIG_WIDTH = TRACK_WIDTH / 2.0 + 2.0 * math.sqrt(2) * SOCKET_WIDTH
 SMALL_WIDTH = BIG_WIDTH - 4.0 * SOCKET_WIDTH - 4.0 * math.sqrt(2) * SOCKET_WIDTH
 RATIO = SMALL_WIDTH / (SMALL_WIDTH + BIG_WIDTH)
 ACCELLERATION = 1
-TIMESTEP = .015
-ACCELLERATION_CONST = ACCELLERATION * TIMESTEP
 
 
 def falling(car):
@@ -22,13 +20,13 @@ def falling(car):
     else:
         c = RATIO
         threshold = 1 + (c * math.cos(scale_big_loop(d, c)))
-    return car.speed, threshold
+    return car.speed > threshold
 
 def colliding(d):
     return False
 
 def calculate_posn(car):
-    c = RATIO if car.get_id() == 0 else 1 - RATIO
+    c = RATIO if car.id == 0 else 1 - RATIO
     d = car.distance
     curr_width, scale_fn = ((BIG_WIDTH, scale_big_loop) if d >= c
         else (SMALL_WIDTH, scale_small_loop))
@@ -45,16 +43,17 @@ def scale_small_loop(d, c):
 def scale_big_loop(d, c):
     return math.pi / 2.0 + ((d - c) * math.pi) / (1 - c)
 
-def calculate_velocity(speed, accellerating):
+def calculate_speed(speed, accellerating, timestep):
+    acceleration = ACCELLERATION * timestep
     if accellerating:
-        return min(speed + ACCELLERATION_CONST, MAX_SPEED)
+        return min(speed + acceleration, MAX_SPEED)
     else:
-        return max(speed - ACCELLERATION_CONST, 0)
+        return max(speed - acceleration, 0)
 
-def calculate_distance(distance, speed):
-    return distance + (speed * TIMESTEP) #TODO: temporary, might want to use method similar to in `falling`
+def calculate_distance(distance, speed, timestep):
+    return distance + (speed * timestep) #TODO: temporary, might want to use method similar to in `falling`
 
-def car_timestep(car):
-    speed = calculate_velocity(car.speed, car.accellerating)
-    distance = calculate_distance(car.distance, car.speed)
+def car_timestep(car, timestep):
+    speed = calculate_speed(car.speed, car.is_accelerating, timestep)
+    distance = calculate_distance(car.distance, car.speed, timestep)
     return speed, distance
